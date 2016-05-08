804b71f0a6420ad00e19c45343522858f7224e46
MikeGalli <mikegalligar@mikes-mbp-2.seattleu.edu>
2016-05-08 16:33:32 -0700

    df

--- js/main.js
+++ js/main.js
@@ -10,15 +10,14 @@ $(document).on('ready', function(){
     (function() {
       var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
       $.getJSON( flickerAPI, {
-        tags: tags,
+        tags: "mount rainier",
         tagmode: "any",
         format: "json"
       })
         .done(function( data ) {
           $.each( data.items, function( i, item ) {
-            $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-            if ( i === 3 ) {
-              return false;
+
+
             }
           });
         });
